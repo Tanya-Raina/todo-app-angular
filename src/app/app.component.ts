c12b445b48c407c62a3ac75a4783ec8d2005a523
Tanya-Raina <rainatanya21@gmail.com>
2022-09-13 12:12:38 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Guid } from "guid-typescript";
+import {Todo} from "../models/todo.model";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-app';
+
+  todos: Todo[] = [
+    new Todo(Guid.create(), 'Learn Angular', false),
+    new Todo(Guid.create(), 'Learn Typescript', false),
+  ]
+
+  onSubmit(form: NgForm) {
+    let todo = new Todo(Guid.create(), form.value.title, false);
+    this.todos = [...this.todos, todo];
+    form.reset();
+  }
+
+  onComplete(id: Guid) {
+    let todo = this.todos.filter(todo => todo.id === id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+}
